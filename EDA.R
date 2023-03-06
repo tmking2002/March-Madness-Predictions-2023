28c8f920e92d5bc983c1fc985e2128272ccc9190
tmking2002 <tmking4@ncsu.edu>
2023-03-06 11:43:04 -0500

    update

--- EDA.R
+++ EDA.R
@@ -5,6 +5,8 @@ file_names <- list.files("data")
 files <- lapply(paste0("data/",file_names), read.csv)
 names(files) <- str_remove(file_names, ".csv")
 
+## MENS
+
 mens_scores <- rbind(files$MRegularSeasonDetailedResults[c(1:4,8:21)] %>% `names<-`(str_remove_all(names(.),"W|L")),
                     files$MRegularSeasonDetailedResults[c(1,2,5:6,8,22:34)] %>% `names<-`(str_remove_all(names(.),"W|L")))
 
@@ -13,7 +15,7 @@ mens_scores <- cbind(mens_scores, rbind(files$MRegularSeasonDetailedResults[c(1,
   merge(files$MTeamConferences, by = c("Season","TeamID")) %>% 
   merge(files$MTeamConferences %>% rename(opp_ConfAbbrev = ConfAbbrev), by.x = c("Season","opp_TeamID"), by.y = c("Season","TeamID"))
 
-conf_stats <- mens_scores %>% 
+mens_conf_stats <- mens_scores %>% 
   group_by(Season, ConfAbbrev, opp_ConfAbbrev) %>% 
   summarise(ppg = mean(Score),
             opp_ppg = mean(opp_Score),
@@ -27,7 +29,7 @@ mens_ranking_data <- files$MMasseyOrdinals %>%
   select(-c(SystemName, RankingDayNum))
 
 mens_tournament_conf_stats <- mens_ranking_data %>% 
-  merge(conf_stats, by = c("Season","ConfAbbrev")) %>% 
+  merge(mens_conf_stats, by = c("Season","ConfAbbrev")) %>% 
   select(Season, ConfAbbrev, opp_ConfAbbrev, conf_ppg_diff) %>% 
   distinct()
 
@@ -68,4 +70,69 @@ mean(mens_tourney_test[1:303,]$Resid)
 mens_submission <- mens_tourney_test %>% 
   mutate(ID = paste0(Season,"-",WTeamID,"-",LTeamID)) %>% 
   select(ID, pred)
+
+## WOMENS
+
+womens_scores <- rbind(files$WRegularSeasonDetailedResults[c(1:4,8:21)] %>% `names<-`(str_remove_all(names(.),"W|L")),
+                       files$WRegularSeasonDetailedResults[c(1,2,5:6,8,22:34)] %>% `names<-`(str_remove_all(names(.),"W|L")))
+
+womens_scores <- cbind(womens_scores, rbind(files$WRegularSeasonDetailedResults[c(1,2,5:6,8,22:34)] %>% `names<-`(paste0("opp_",str_remove_all(names(.),"W|L"))),
+                                            files$WRegularSeasonDetailedResults[c(1:4,8:21)] %>% `names<-`(paste0("opp_",str_remove_all(names(.),"W|L"))))) %>% 
+  merge(files$WTeamConferences, by = c("Season","TeamID")) %>% 
+  merge(files$WTeamConferences %>% rename(opp_ConfAbbrev = ConfAbbrev), by.x = c("Season","opp_TeamID"), by.y = c("Season","TeamID"))
+
+womens_conf_stats <- womens_scores %>% 
+  group_by(Season, ConfAbbrev, opp_ConfAbbrev) %>% 
+  summarise(ppg = mean(Score),
+            opp_ppg = mean(opp_Score),
+            conf_ppg_diff = ppg - opp_ppg)
+
+womens_stats <- womens_scores %>% 
+  mutate(win = Score > opp_Score,
+         margin = Score - opp_Score) %>% 
+  group_by(TeamID, Season, ConfAbbrev) %>% 
+  summarise(win_perc = mean(win),
+            avg_margin = mean(margin))
+
+womens_tournament_conf_stats <- womens_stats %>% 
+  merge(womens_conf_stats, by = c("Season","ConfAbbrev")) %>% 
+  select(Season, ConfAbbrev, opp_ConfAbbrev, conf_ppg_diff) %>% 
+  distinct()
+
+womens_tourney_results <- files$WNCAATourneyDetailedResults %>% 
+  select(Season, WTeamID, LTeamID, WScore, LScore) %>% 
+  merge(womens_stats, by.x = c("Season","WTeamID"), by.y = c("Season","TeamID")) %>% 
+  rename(Wwin_perc = win_perc,
+         Wavg_margin = avg_margin,
+         WConfAbbrev = ConfAbbrev) %>% 
+  merge(womens_stats, by.x = c("Season","LTeamID"), by.y = c("Season","TeamID")) %>% 
+  rename(Lwin_perc = win_perc,
+         Lavg_margin = avg_margin,
+         LConfAbbrev = ConfAbbrev) %>% 
+  mutate(win_perc_diff = Wwin_perc - Lwin_perc,
+         margin_diff = Wavg_margin - Lavg_margin) %>% 
+  merge(womens_tournament_conf_stats, by.x = c("Season","WConfAbbrev","LConfAbbrev"), by.y = c("Season","ConfAbbrev","opp_ConfAbbrev"))
+
+womens_tourney_results_longer <- rbind(womens_tourney_results %>% mutate(win = 1, score_diff = WScore - LScore),
+                                     womens_tourney_results[c(1,3,2,5,4,7,6,9,8,10,11,12,13,14)] %>% mutate(win_perc_diff = -win_perc_diff,
+                                                                                                    margin_diff = -margin_diff,
+                                                                                                    conf_ppg_diff = -conf_ppg_diff,
+                                                                                                    score_diff = LScore - WScore,
+                                                                                                    win = 0))
+
+womens_tourney_test <- womens_tourney_results_longer %>% 
+  filter(Season %in% c(2017, 2018, 2019, 2021, 2022))
+
+womens_tourney_train <- womens_tourney_results_longer %>% 
+  filter(!(Season %in% c(2017, 2018, 2019, 2021, 2022)))
+
+womens_model <- glm(score_diff ~ margin_diff + win_perc_diff + conf_ppg_diff, data = womens_tourney_train, family = "gaussian")
+womens_point_sd <- sd(c(womens_tourney_train$WScore, womens_tourney_train$LScore))
+
+womens_tourney_test$predicted_point_diff <- predict(womens_model, womens_tourney_test, type = "response")
+womens_tourney_test$pred <- pnorm(womens_tourney_test$predicted_point_diff, 0, womens_point_sd)
+
+womens_tourney_test$Resid <- womens_tourney_test$win - womens_tourney_test$pred
+
+mean(womens_tourney_test[1:554,]$Resid)
 
--- create_submission.R
+++ create_submission.R
@@ -1,41 +1,106 @@
 source("EDA.R")
 
-teams <- files$MRegularSeasonCompactResults %>% 
-  filter(Season %in% c(2017:2019,2021:2022)) %>% 
-  distinct(Season, LTeamID) %>% 
-  rename(TeamID = LTeamID)
+## MENS
 
+mens_teams <- data.frame()
 
-matchups <- data.frame()
-
-for(i in unique(teams$Season)){
+for(i in c(2017:2019,2021:2022)){
   
-  curr_matchups <- expand.grid(teams %>% filter(Season == i) %>% pull(TeamID),
-                               teams %>% filter(Season == i) %>% pull(TeamID))
+  teams <- files$MTeams %>% 
+    filter(i >= FirstD1Season & i <= LastD1Season) %>% 
+    pull(TeamID)
   
-  matchups <- rbind(matchups, curr_matchups %>% mutate(season = i))
+  mens_teams <- rbind(mens_teams, data.frame(Season = i,
+                                             TeamID = teams))
+}
+
+
+mens_matchups <- data.frame()
+
+for(i in unique(mens_teams$Season)){
+  
+  curr_matchups <- expand.grid(mens_teams %>% filter(Season == i) %>% pull(TeamID),
+                               mens_teams %>% filter(Season == i) %>% pull(TeamID))
+  
+  mens_matchups <- rbind(mens_matchups, curr_matchups %>% mutate(season = i))
   
 }
 
-matchups <- matchups %>% 
-  filter(Var1 != Var2) %>% 
+mens_matchups <- mens_matchups %>% 
+  filter(Var1 < Var2) %>% 
   rename(Team1 = Var1,
          Team2 = Var2)
 
-team_data <- matchups %>% 
+mens_team_data <- mens_matchups %>% 
   merge(mens_ranking_data, by.x = c("season","Team1"), by.y = c("Season","TeamID")) %>% 
   rename(Rank1 = OrdinalRank,
          Conf1 = ConfAbbrev) %>% 
   merge(mens_ranking_data, by.x = c("season","Team2"), by.y = c("Season","TeamID")) %>% 
   rename(Rank2 = OrdinalRank,
          Conf2 = ConfAbbrev) %>% 
-  merge(conf_stats, by.x = c("season","Conf1","Conf2"), by.y = c("Season","ConfAbbrev","opp_ConfAbbrev"), all = T) %>% 
+  merge(mens_conf_stats, by.x = c("season","Conf1","Conf2"), by.y = c("Season","ConfAbbrev","opp_ConfAbbrev"), all = T) %>% 
   filter(season %in% c(2017, 2018, 2019, 2021, 2022)) %>% 
   mutate(conf_ppg_diff = ifelse(is.na(conf_ppg_diff), 0, conf_ppg_diff),
          rank_diff = Rank1 - Rank2)
 
-test <- team_data %>% select(season, Team1, Team2)
+mens_team_data$predicted_point_diff <- predict(rank_model, mens_team_data, type = "response")
+mens_team_data$pred <- pnorm(mens_team_data$predicted_point_diff, 0, point_sd)
 
+mens_submission <- mens_team_data %>% 
+  mutate(ID = paste0(season,"_",Team1,"_",Team2)) %>% 
+  select(ID, pred)
 
-team_data$predicted_point_diff <- predict(rank_model, team_data, type = "response")
-team_data$pred <- pnorm(team_data$predicted_point_diff, 0, point_sd)
+## WOMENS
+
+womens_teams <- data.frame()
+
+for(i in c(2017:2019,2021,2022)){
+  
+  teams <- files$WTeams %>% 
+    pull(TeamID)
+  
+  womens_teams <- rbind(womens_teams, data.frame(Season = i,
+                                             TeamID = teams))
+}
+
+womens_matchups <- data.frame()
+
+for(i in unique(womens_teams$Season)){
+  
+  curr_matchups <- expand.grid(womens_teams %>% filter(Season == i) %>% pull(TeamID),
+                               womens_teams %>% filter(Season == i) %>% pull(TeamID))
+  
+  womens_matchups <- rbind(womens_matchups, curr_matchups %>% mutate(season = i))
+  
+}
+
+womens_matchups <- womens_matchups %>% 
+  filter(Var1 < Var2) %>% 
+  rename(Team1 = Var1,
+         Team2 = Var2)
+
+womens_team_data <- womens_matchups %>% 
+  merge(womens_stats, by.x = c("season","Team1"), by.y = c("Season","TeamID")) %>% 
+  rename(win_perc_1 = win_perc,
+         margin_1 = avg_margin,
+         Conf1 = ConfAbbrev) %>% 
+  merge(womens_stats, by.x = c("season","Team2"), by.y = c("Season","TeamID")) %>% 
+  rename(win_perc_2 = win_perc,
+         margin_2 = avg_margin,
+         Conf2 = ConfAbbrev) %>% 
+  merge(womens_conf_stats, by.x = c("season","Conf1","Conf2"), by.y = c("Season","ConfAbbrev","opp_ConfAbbrev"), all = T) %>% 
+  filter(season %in% c(2017, 2018, 2019, 2021, 2022)) %>% 
+  mutate(conf_ppg_diff = ifelse(is.na(conf_ppg_diff), 0, conf_ppg_diff),
+         win_perc_diff = win_perc_1 - win_perc_2,
+         margin_diff = margin_1 - margin_2)
+
+womens_team_data$predicted_point_diff <- predict(womens_model, womens_team_data, type = "response")
+womens_team_data$pred <- pnorm(womens_team_data$predicted_point_diff, 0, womens_point_sd)
+
+womens_submission <- womens_team_data %>% 
+  mutate(ID = paste0(season,"_",Team1,"_",Team2)) %>% 
+  select(ID, pred)
+
+submission <- rbind(womens_submission, mens_submission) %>% 
+  arrange(ID)
+
